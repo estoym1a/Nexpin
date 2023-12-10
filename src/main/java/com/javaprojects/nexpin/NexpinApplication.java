c5d9afb0bf4c1fbf4090b3ac7a9d9f03f606758a
estoym1a <nuray.xudadatova04@gmail.com>
2023-12-10 12:06:53 +0400

    "build gradle"

---
+++ src/main/java/com/javaprojects/nexpin/NexpinApplication.java
@@ -0,0 +1,13 @@
+package com.javaprojects.nexpin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexpinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexpinApplication.class, args);
+    }
+
+}
