fef66c2bfa916f229a6dfea493ba3e64cb8fdc7d
estoym1a <nuray.xudadatova04@gmail.com>
2023-12-14 13:26:59 +0400

    "Client Requests and its Constants"

---
+++ src/main/java/com/javaprojects/nexpin/model/constant/Constants.java
@@ -0,0 +1,10 @@
+package com.javaprojects.nexpin.model.constant;
+
+public class Constants {
+    public final static String USERNAME_URGENT="Enter Username";
+    public final static String FULLNAME_URGENT="Enter FullName";
+    public final static String INVALID_PHONE="Enter Phone Number";
+    public final static String EMAIL_IS_NOT_VALID="Enter a Valid Email";
+    public final static String PASSWORD_URGENT="Enter Password";
+    public final static String PASSWORD_REGEX = "Enter at least 8 characters. At least one uppercase letter, one number, and one special character.";
+}
---
+++ src/main/java/com/javaprojects/nexpin/model/dto/request/ClientRequest.java
@@ -0,0 +1,38 @@
+package com.javaprojects.nexpin.model.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.validation.annotation.Validated;
+
+import static com.javaprojects.nexpin.model.constant.Constants.*;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Validated
+public class ClientRequest {
+    @NotBlank(message = USERNAME_URGENT)
+    String userName;
+
+    @NotBlank(message = FULLNAME_URGENT)
+    String fullName;
+
+    @NotBlank(message = EMAIL_IS_NOT_VALID)
+    @Email(message = EMAIL_IS_NOT_VALID)
+    String email;
+
+    @Pattern(regexp = "^(\\+[0-9]{1,4}[-.\\s]?)?((\\([0-9]{1,4}\\))|[0-9]{1,4})[-.\\s]?[0-9]{1,14}$",
+            message = INVALID_PHONE)
+    @NotBlank(message = INVALID_PHONE)
+    String phoneNumber;
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "PASSWORD_REGEX")
+    @NotBlank(message = PASSWORD_URGENT)
+    String password;
+}
