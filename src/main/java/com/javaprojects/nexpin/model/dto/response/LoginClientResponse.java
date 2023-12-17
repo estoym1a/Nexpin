af43c24d09b200d27e731fdc18120c69b5c4e535
estoym1a <nuray.xudadatova04@gmail.com>
2023-12-17 12:48:18 +0400

    "Login Client Response"

---
+++ src/main/java/com/javaprojects/nexpin/model/dto/response/LoginClientResponse.java
@@ -0,0 +1,18 @@
+package com.javaprojects.nexpin.model.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.validation.annotation.Validated;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Validated
+public class LoginClientResponse {
+    Long id;
+    String username;
+    String token;
+
+}
