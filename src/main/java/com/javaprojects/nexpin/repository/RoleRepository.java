54a92e93ad00b81a696d4690104762f14332d39a
estoym1a <nuray.xudadatova04@gmail.com>
2023-12-24 13:15:56 +0400

    "Role Repo"

--- src/main/java/com/javaprojects/nexpin/repository/RoleRepository.java
+++ src/main/java/com/javaprojects/nexpin/repository/RoleRepository.java
@@ -1,6 +1,5 @@
 package com.javaprojects.nexpin.repository;
 
-import com.javaprojects.nexpin.model.entity.AccountType;
 import com.javaprojects.nexpin.model.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
