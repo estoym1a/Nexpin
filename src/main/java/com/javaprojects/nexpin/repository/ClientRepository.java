0c42457336aadce926d2f4260a86278dcbb4ab4e
estoym1a <nuray.xudadatova04@gmail.com>
2024-01-04 12:58:05 +0400

    "Editing userName error"

--- src/main/java/com/javaprojects/nexpin/repository/ClientRepository.java
+++ src/main/java/com/javaprojects/nexpin/repository/ClientRepository.java
@@ -3,5 +3,8 @@ package com.javaprojects.nexpin.repository;
 import com.javaprojects.nexpin.model.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client,Long> {
+    Optional<Client> findByUserName(String userName); //The username might not exist in the database. So that we use optional not to return 'NullPointerException'.
 }
