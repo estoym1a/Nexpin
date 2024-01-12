e3689b4ff2319572d31e3c586e983715371a3a27
estoym1a <nuray.xudadatova04@gmail.com>
2024-01-12 21:25:01 +0400

    "Removing TransactionType, TransactionDetails entities and repositories"

--- src/main/java/com/javaprojects/nexpin/repository/TransactionDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javaprojects.nexpin.repository;
-
-import com.javaprojects.nexpin.model.entity.TransactionDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionDetailsRepository extends JpaRepository<TransactionDetails,Long> {
-}
--- src/main/java/com/javaprojects/nexpin/repository/TransactionTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javaprojects.nexpin.repository;
-
-import com.javaprojects.nexpin.model.entity.Account;
-import com.javaprojects.nexpin.model.entity.TransactionType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionTypeRepository extends JpaRepository<TransactionType,Long> {
-}
