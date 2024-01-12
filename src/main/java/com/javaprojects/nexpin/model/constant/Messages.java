a8b0b6958e5f542211a105c7f3ee7ad8e42f053c
estoym1a <nuray.xudadatova04@gmail.com>
2024-01-12 22:14:48 +0400

    "Editing the name of the class Constants to Messages"

---
+++ src/main/java/com/javaprojects/nexpin/model/constant/Messages.java
@@ -0,0 +1,22 @@
+package com.javaprojects.nexpin.model.constant;
+
+public class Messages {
+    public final static String FULLNAME_URGENT = "Enter FullName";
+    public final static String INVALID_PHONE = "Enter Phone Number";
+    public final static String PASSWORD_URGENT = "Enter Password";
+    public final static String PASSWORD_REGEX = "Enter at least 8 characters. At least one uppercase letter, one number, and one special character.";
+    public final static String ID_URGENT = "Enter Id";
+    public final static String ACCOUNT_TYPE_URGENT = "Enter Account Type";
+    public final static String CURRENCY_URGENT = "Enter Currency";
+    public final static String ACCOUNT_ID_URGENT = "Enter Account Id";
+    public final static String AMOUNT_URGENT = "Enter Amount";
+    public final static String TRANSACTION_TYPE_URGENT = "Enter Transaction Type";
+    public final static String CLIENT_ID_URGENT = "Enter Client Id";
+    public final static String ACTION_URGENT = "Enter an Action Type";
+    public final static String EXCEED_LIMIT = "The amount cannot exceed 1000.";
+    public final static String CLIENT_NOT_FOUND = "Client Not Found";
+    public final static String INVALID_DEPOSIT="Deposit amount must be positive";
+    public final static String INVALID_WITHDRAWAL="Invalid withdrawal amount or insufficient balance";
+
+
+}
